616988ab0b1bf96350a1baa7b2b9bf8fcb198156
jose luizz <lui_zzzz@hotmail.com>
2024-10-22 17:43:27 -0300

    Refatorar configuração do Vite para adicionar limite de aviso de tamanho de chunk

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,13 @@ export default defineConfig({
         },
     },
     build: {
+      rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
        chunkSizeWarningLimit: 1000,
     },
     esbuild: {
